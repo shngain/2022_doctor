404e2be8e9eb44ea269a2d682e6e7613171be87a
shngain <mpynzhng@gmail.com>
2022-09-19 12:43:12 +0530

    09192022

--- src----/app/_pipes/human-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HumanDatePipe } from './human-date.pipe';
-
-describe('HumanDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new HumanDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
